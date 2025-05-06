2a458c2b2fef90ddcbeef5e843b0f091a1a86fb6
sachidanadpc <sachidanandpc@gmail.com>
2025-05-06 23:31:52 +0530

    updated PetEndpints code for log

--- src/test/java/api/endpoints/PetEndPoints.java
+++ src/test/java/api/endpoints/PetEndPoints.java
@@ -69,13 +69,6 @@ public class PetEndPoints {
 	public static Response DeletePetData(int petId)	
 	{
 		
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
 		System.out.println("Delete Pet Data End points "+ petId);
 		
 		
